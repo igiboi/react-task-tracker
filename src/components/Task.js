34568c9d922e6bf8da8ac1e98844f5f29356a21c
luigi <igiboi26@gmail.com>
2021-06-15 13:26:56 -0400

    Task Tracker

---
+++ src/components/Task.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const Task = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Grocery Shopping",
+      day: "Feb 7th at 12:30pm",
+      reminder: true,
+    },
+  ]);
+  return (
+    <>
+      {tasks.map((task) => (
+        <h3 key={task.id}>{task.text}</h3>
+      ))}
+    </>
+  );
+};
+
+export default Task;
